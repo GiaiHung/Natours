f849fa2c3ddf4b5acde99bfa9e431d2cccb9b541
GiaiHung <91544801+GiaiHung@users.noreply.github.com>
2023-01-04 08:14:41 +0700

    rate-limit, http headers, sanitization, XSS, hpp

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ require('colors')
 const express = require('express')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
+const rateLimit = require('express-rate-limit')
+const helmet = require('helmet')
+const mongoSanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean')
+const hpp = require('hpp')
 const route = require('./routes')
 const connect = require('./db/connect')
 const handleRejection = require('./utils/handleRejections')
@@ -11,8 +16,42 @@ const handleRejection = require('./utils/handleRejections')
 handleRejection()
 const app = express()
 const PORT = process.env.PORT || 5000
+const limiter = rateLimit({
+  max: 100,
+  message: 'Too many requests from this IP. Please try again in 30 minutes.',
+  windowMs: 30 * 60 * 1000,
+})
+const passwordLimiter = rateLimit({
+  max: 5,
+  message:
+    'You have tried logged in so many times. Please try again in 10 minutes.',
+  windowMs: 10 * 60 * 1000,
+})
 
-app.use(express.json())
+// Set secure HTTP headers
+app.use(helmet())
+// Set limit on requests
+app.use('/api', limiter)
+app.use('/api/v1/auth/login', passwordLimiter)
+// Parse the body from request into usable req.body, limit the data receive
+app.use(express.json({ limit: '20kb' }))
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize())
+// Data sanitization against XSS, prevent HTML or JS code
+app.use(xss())
+// Prevent http parameters pollution, duplicate sort,...
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+)
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
