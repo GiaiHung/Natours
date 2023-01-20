2a27c885bd50f0a9519c1e2d89297f15a741a61e
GiaiHung <91544801+GiaiHung@users.noreply.github.com>
2023-01-20 09:12:53 +0700

    stripe webhook fix

--- controllers/booking/index.js
+++ controllers/booking/index.js
@@ -10,7 +10,7 @@ const getCheckourSession = catchAsync(async (req, res, next) => {
   const tour = await Tours.findById(req.params.tourId)
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}/`,
+    success_url: `${req.protocol}://${req.get('host')}/my-tours`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
@@ -44,7 +44,7 @@ const getCheckourSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = catchAsync(async (session) => {
   const tour = session.client_reference_id
   const user = (await User.findOne({ email: session.customer_email }))._id
-  const price = session.line_items[0].price_data.unit_amount / 100
+  const price = session.amount_total / 100
   await Booking.create({ tour, user, price })
 })
 
